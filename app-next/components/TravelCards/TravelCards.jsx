58c8f8b99915f5fc9e9f0b050e59c08c278b3a05
nigerabed <nigerabed@gmail.com>
2025-08-31 01:18:23 +0200

    displayed just 3 trips in the front page by slice the data

--- app-next/components/TravelCards/TravelCards.jsx
+++ app-next/components/TravelCards/TravelCards.jsx
@@ -1,12 +1,35 @@
-"use client"; // if in Next.js app dir
+"use client"; 
 import { useEffect, useState } from "react";
 import styles from "./TravelCards.module.css";
 import traveldata from "../../mockData/travel-cards.json";
 import Card from "../Card/Card";
 import Link from "next/link";
+import globalcss from '../../app/globals.css';
 
 export default function TravelCards() {
   const [travelCardData, setTravelCardData] = useState([]);
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState(null);
+  
+  // useEffect(() => {
+  //   async function fetchTravelData() {
+  //     try {
+  //       const res = await fetch("/api/travel-cards"); // 👈 replace with your API endpoint
+  //       if (!res.ok) {
+  //         throw new Error("Failed to fetch travel data");
+  //       }
+  //       const data = await res.json();
+  //       setTravelCardData(data);
+  //     } catch (err) {
+  //       setError(err.message);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   }
+
+  //   fetchTravelData();
+  // }, []);
+
 
   // simulate API fetch
   useEffect(() => {
@@ -16,14 +39,17 @@ export default function TravelCards() {
 
   return (
     <>
+    <section className={`${styles.travelSection}`}>
+  <h2 className={`container ${styles.sectionTitle}`}>Explore Your Trips</h2>
       <div className={styles.gridContainer}>
-        {travelCardData.map((card) => (
+        {travelCardData.slice(0, 3).map((card) => (
           <Card key={card.id} card={card} />
         ))}
       </div>
       <Link href="/trips" style={{ textDecoration: "none" }}>
         <button className={styles.exploreBtn}>Explore more</button>
       </Link>
+      </section>
     </>
   );
 }
