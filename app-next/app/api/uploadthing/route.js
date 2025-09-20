90708516d7dcd3496cf2e3115782c1be9f9ee4b0
ekarkooti <ekarkooti@gmail.com>
2025-09-20 17:51:39 +0200

    add debugging to find uploadthing issue

--- app-next/app/api/uploadthing/route.js
+++ app-next/app/api/uploadthing/route.js
@@ -1,6 +1,27 @@
 import { createRouteHandler } from "uploadthing/next";
 import { ourFileRouter } from "./core";
 
+// ======================= DEFINITIVE DEBUGGING START =======================
+console.log(
+  "\n[UploadThing DEBUG] SERVER STARTUP: Checking Environment Variables..."
+);
+console.log(
+  "[UploadThing DEBUG] UPLOADTHING_SECRET is set:",
+  !!process.env.UPLOADTHING_SECRET
+);
+console.log(
+  "[UploadThing DEBUG] UPLOADTHING_APP_ID is set:",
+  !!process.env.UPLOADTHING_APP_ID
+);
+console.log(
+  "[UploadThing DEBUG] UPLOADTHING_URL value is:",
+  process.env.UPLOADTHING_URL
+);
+console.log(
+  "[UploadThing DEBUG] Environment check complete on server startup.\n"
+);
+// ======================== DEFINITIVE DEBUGGING END ========================
+
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
 });
