0233fdc1cdd9956077135b7207d194540180975d
ekarkooti <ekarkooti@gmail.com>
2025-09-20 21:08:09 +0200

    fix5 for image upload

--- app-next/utils/uploadthing.js
+++ app-next/utils/uploadthing.js
@@ -1,6 +1,6 @@
 // app/utils/uploadthing.js
 import { generateReactHelpers } from "@uploadthing/react/generate-react-helpers";
 
-import { ourFileRouter } from "../api/uploadthing/core";
+import { ourFileRouter } from "../app/api/uploadthing/core";
 
 export const { useUploadThing } = generateReactHelpers();
